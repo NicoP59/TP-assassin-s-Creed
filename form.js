bfa4f8a1f7da4e49a38eee3a01ed40e94c7b189d
NicoP59 <nicolaspayne06@gmail.com>
2021-12-01 12:42:14 +0100

    form

---
+++ form.js
@@ -0,0 +1,25 @@
+const message = "Thank you for fight or die.";
+
+document
+  .getElementById("contact_form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
+
+//   ************************dark mode
+
+let theme_toggler = document.querySelector("#logo-ac");
+
+let count = 1;
+theme_toggler.addEventListener("click", function () {
+  document.body.classList.toggle("assassin_mode");
+  count++;
+  if (count % 2 === 0) {
+    document.getElementById("logo-ac").src = "img/drapeau-usa.png";
+    document.body.style.backgroundImage = "url('img/fond_american.jpg')";
+  } else {
+    document.getElementById("logo-ac").src = "img/logo.png";
+    document.body.style.backgroundImage = "url('img/war.jpg')";
+  }
+});
